4ab417b3869cdee872da2056d0969d6a7ce7b84e
Haicheng Charles Zhao <czhao39@gmail.com>
2017-03-07 17:20:31 -0500

    update install script

--- post_install.sh
+++ post_install.sh
@@ -1,28 +1,35 @@
 #!/usr/bin/env sh
-echo 'installing ros'
+
+ROS_DISTRO=kinetic
+
+echo "Installing ROS distro $ROS_DISTRO"
 sudo apt-get update --force-yes
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
 
 sudo apt-get --force-yes update
-sudo apt-get --force-yes install libgl1-mesa-dev-lts-utopic
-sudo apt-get --force-yes install vim vim-runtime
-sudo apt-get --force-yes install tmux 
-sudo apt-get --force-yes install git 
-sudo apt-get --force-yes install ros-kinetic-desktop-full
-sudo apt-get --force-yes install ros-kinetic-ackermann-msgs ros-kinetic-serial
-
+sudo apt-get --force-yes install python-wstool libgl1-mesa-dev-lts-utopic vim tmux screen git curl wget ros-$ROS_DISTRO-desktop-full
 sudo apt-get --force-yes update
 
+echo "Creating racecar-ws workspace"
 mkdir -p ~/racecar-ws/src
 cd ~/racecar-ws/src
-git clone https://github.com/mit-racecar/racecar-simulator.git
-git clone https://github.com/mit-racecar/racecar.git
-git clone https://github.com/Bobobalink/vesc.git  # use the vesc package with changes for newer versions of boost
+source /opt/ros/$ROS_DISTRO/setup.bash
+catkin_init_workspace
+cd ~/racecar-ws
+curl -L https://raw.githubusercontent.com/mit-racecar/racecar/master/racecar-vm.rosinstall > .rosinstall
+wstool update
+
+# Update 3rd party ROS deps
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -y -r
+catkin_make
 
 
 ### INSTALL GAZEBO
+echo "Installing Gazebo"
 
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 
@@ -33,32 +40,25 @@ sudo apt-get --force-yes update
 sudo apt-get --force-yes install ros-kinetic-controller-manager ros-kinetic-gazebo-ros-control ros-kinetic-gazebo-ros-pkgs ros-kinetic-joint-state-controller ros-kinetic-effort-controllers
 
 
-### SET UP WORKSPACE
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-. ~/.bashrc
+echo "Setting up .bashrc"
 
-cd ~/racecar-ws/src/
-catkin_init_workspace
-cd ~/racecar-ws
-catkin_make
-
-echo "source ~/racecar-ws/devel/setup.sh" >> ~/.bashrc
-echo "export ROS_IP=`hostname -I`" >> ~/.bashrc
+cd ~/
+curl -L https://raw.githubusercontent.com/mit-racecar/install_tools/master/bashrc_ending_vm.snippet >> ~/.bashrc
+source ~/.bashrc
 
 
-### INSTALL ZED SDK
+### SET UP ZED SDK
+
+sudo apt-get --force-yes install ros-kinetic-pcl-*
 
 # Jetson TX1
+sudo apt-get --force-yes install libopencv4tegra-python
 wget -O ZED_SDK_v1.2.0.run https://www.stereolabs.com/download_327af3/ZED_SDK_Linux_JTX1_v1.2.0_64b_JetPack23.run
 
 # Ubuntu
 #wget -O ZED_SDK_v1.2.0.run https://www.stereolabs.com/download_327af3/ZED_SDK_Linux_Ubuntu16_CUDA80_v1.2.0.run
 
 sh ZED_SDK_v1.2.0.run
-
-
-### INSTALL PCL
-sudo apt-get --force-yes install ros-kinetic-pcl-*
 
 
 ### INSTALL URG NODE FOR HOKUYO
